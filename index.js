2cf35f2b33422191775f63a76c8969d64cc0181f
Amer <ameur.bellil@eniso.u-sousse.tn>
2024-08-19 13:38:34 +0100

    [fixed]  small issue

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-
 const thmasButton = document.querySelector('#thmas-button');
 const thmasInputText = document.querySelector('#thmas-input-text');
 
 thmasButton.addEventListener('click', clickButton);
 
 function clickButton() {
-var URL = "https://tayssirgh.github.io/puzzle2-" + thmasInputText.value + "753_/";
+    var URL = "https://ameur-bellil.github.io/puzzle2/" ;
+    var correctinput = parseInt(thmasInputText.value);
 
-var win = window.open(URL, "_blank");
+    if (correctinput === 231) {
+        window.location.href = URL;
+    }else{
+        window.alert("Try again")
+    }
+
+
 }
+
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -33,7 +40,7 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -41,15 +48,10 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-
-
-
-
-
 const result_box = document.querySelector(".result_box");
 const continue_btn = document.querySelector(".buttons .start");
-continue_btn.onclick = ()=>{
-    
-    result_box.classList.add("opacity"); 
-   
+continue_btn.onclick = () => {
+
+    result_box.classList.add("opacity");
+
 }
